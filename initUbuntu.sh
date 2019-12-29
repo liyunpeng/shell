224393d0879bd9cdbe625e735681bdc93d689f4d
k8s master <kylin27@126.com>
2019-12-29 10:31:33 +0800

    update

--- initUbuntu.sh
+++ initUbuntu.sh
@@ -4,7 +4,9 @@ username=`ls /home/`
 sudo apt-get install git -y
 sudo apt-get install vim -y
 sudo apt-get install curl -y
+sudo apt-get install dos2unix -y
 
 git config --global user.email "kylin@126.com"
 git config --global user.name ${ussername} 
+
 
--- k8s/rmaptLock.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo rm /var/cache/apt/archives/lock
-sudo rm /var/lib/dpkg/lock
-sudo rm -rf /var/lib/dpkg/lock-frontend
-
