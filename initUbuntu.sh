4b04ffd2549a7538ae90f5f0c8894b1121b0ca05
master <kylin@126.com>
2019-12-29 11:19:18 +0800

    update init ubuntu

--- initUbuntu.sh
+++ initUbuntu.sh
@@ -7,6 +7,11 @@ sudo apt-get install curl -y
 sudo apt-get install dos2unix -y
 
 git config --global user.email "kylin@126.com"
-git config --global user.name ${ussername} 
+git config --global user.name ${username} 
 
-
+sudo cat << EOF >> /home/${username}/.bashrc
+GOROOT=/home/master/go/go/
+export GOROOT=/home/master/go/go/
+alias ll="ls -l"
+alias k="kubectl"
+EOF
